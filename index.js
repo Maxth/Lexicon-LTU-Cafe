82454ebccc01aa5a5a609936944831785be95198
maxth <maxthorstorp@gmail.com>
2024-06-27 11:35:24 +0200

    fix bg-images, remove obl css, use relative instead of margin

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const hot = document.getElementById('hot-link');
 const juicy = document.getElementById('juicy-link');
 const cosy = document.getElementById('cosy-link');
 
-const hotSection = document.getElementById('first');
-const juicySection = document.getElementById('second');
-const cosySection = document.getElementById('last');
+const hotSection = document.getElementById('hot');
+const juicySection = document.getElementById('juicy');
+const cosySection = document.getElementById('cosy');
 
 hot.addEventListener('click', () => {
   hotSection.scrollIntoView({behavior: 'smooth'});
